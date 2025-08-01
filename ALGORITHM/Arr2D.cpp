ea6f9f7ee350b5947a6489a72a3151b0b622c9fb
praptipatel7 <patelprapti0737@gmail.com>
2025-08-01 13:54:37 +0530

    Data Structure Array 2D

--- ALGORITHM/Arr2D.cpp
+++ ALGORITHM/Arr2D.cpp
@@ -29,19 +29,29 @@
 
 
 //2D Vector
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+
+//     for(int i=0 ; i<mat.size(); i++){
+//         for(int j=0 ; j<mat[i].size();j++){
+//             cout<<mat[i][j]<<"  ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+
+// }
+
+
+
+
+
+//Search in 2D vector
 #include<iostream>
 #include<vector>
 using namespace std;
 
-int main(){
-    vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
-
-    for(int i=0 ; i<mat.size(); i++){
-        for(int j=0 ; j<mat[i].size();j++){
-            cout<<mat[i][j]<<"  ";
-        }
-        cout<<endl;
-    }
-    return 0;
-
-}
